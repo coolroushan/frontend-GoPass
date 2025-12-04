8d30ec33eb2fbdb36dfbd1e680eb9d1c6df885b8
coolroushan <roushankumarcool@gmail.com>
2025-12-04 12:36:07 +0530

    final-push

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
 
         {/* --- 404 PAGE --- */}
         <Route path="*" element={<PageNotFound />} />
-        {/* {all routes} */}
+        
       </Routes>
     </Router>
   );
